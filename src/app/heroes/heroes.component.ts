3f36a8678f5afa8ce15412881cf3c5f1589afb95
Josep Otal <j.otal.pares@gmail.com>
2019-01-09 23:41:32 -0600

    Tutorial add routing and http

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {}
 
@@ -22,7 +21,8 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
