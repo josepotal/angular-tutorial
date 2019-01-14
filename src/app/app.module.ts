ca6d58a39f567f7af03edc4237e79cf579b12534
Josep Otal <j.otal.pares@gmail.com>
2019-01-13 23:21:02 -0600

    Adding ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +9,23 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 
+import { reducers } from './state/rootReducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    MyCounterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot(reducers, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/state/rootReducer.ts
@@ -0,0 +1,11 @@
+import { counterReducer } from './counter/counter.reducer';
+
+// Missing  types
+
+// interface AppState {
+//   counterReducer: CounterState
+// }
+
+export const reducers = {
+  counterReducer,
+};
